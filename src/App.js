3b02f9cf9b5a415b5d602fb14b416bf8b7a91035
FAILASOUF <nefsiabdelouahab@gmail.com>
2025-06-15 18:38:14 +0100

    React Todo App

--- src/App.js
+++ src/App.js
@@ -1,24 +1,51 @@
-import logo from './logo.svg';
 import './App.css';
+import TodoList from './components/TodoList';
+import { ThemeProvider,createTheme } from '@mui/material/styles';
+import { TodosContext } from './contexts/todosContext';
+import {v4 as uuidv4} from 'uuid';
+import { useState,React } from 'react';
 
+const theme = createTheme({
+  typography: {
+    fontFamily: 'CustomFont',
+  },
+  palette: {
+    primary: {
+      main: '#ff5722',
+    },
+    secondary: {
+      main: '#dc004e',
+    },
+  },
+ 
+  direction: 'rtl', // Set the direction to right-to-left
+});
 function App() {
+  const [todos, setTodos] = useState([
+    {
+      id: uuidv4(),
+      title : 'مهمة 1',
+      description: 'وصف المهمة 1',
+      isCompleted: false  
+    },
+    {
+      id: uuidv4(),
+      title : 'مهمة 2',
+      description: 'وصف المهمة 2',
+      isCompleted: false  
+    },
+   
+  ]
+  );
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+        <div className="App" style={{ direction: 'rtl', display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh',backgroundColor: '#191b1f' }}>
+      <TodosContext.Provider value={{ todos :todos, setTodos :setTodos }}>
+        <TodoList />
+      </TodosContext.Provider>
+    </div>  
+    </ThemeProvider>
+  
   );
 }
 
